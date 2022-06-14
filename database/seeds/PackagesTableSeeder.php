afb84e38ad68155b7a7e5462b440bd32c32897a2
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-06-14 17:04:59 +0200

    create Controller and Model

---
+++ database/seeds/PackagesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Package;
+
+class PackagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {  
+        $packages = [
+            [
+
+            ]
+        ];
+    }
+}
